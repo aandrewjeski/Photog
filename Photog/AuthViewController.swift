4d21550968ad82dbb8ea86bbeb46ccdcd3149f88
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-10-24 18:38:40 -0400

    Day 8: Add alert logic for invalid entries

--- Photog/AuthViewController.swift
+++ Photog/AuthViewController.swift
@@ -72,17 +72,15 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
     }
     
     func authenticate() {
-        println("authenticate!")
+        var email = self.emailTextField?.text
+        var password = self.passwordTextField?.text
+        
+        if (email?.isEmpty == true || password?.isEmpty == true || email?.isEmailAddress() == false) {
+            self.showAlert("Invalid E-mail")
+            
+            return
+        }
+        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
