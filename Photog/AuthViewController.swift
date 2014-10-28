f4c0db91d94aacfcb0a6cc2cb9c575af347faa76
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-10-28 17:29:07 -0400

    Day 11: Set up foundation for following relation. User follows self.

--- Photog/AuthViewController.swift
+++ Photog/AuthViewController.swift
@@ -127,12 +127,33 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
         
             if error == nil
             {
-                println("sign up success")
+                self.follow(user, completionHandler: {
+                    (error) -> () in
+                    
+                    if error == nil {
+                        var tabBarController = TabBarController()
+                        self.navigationController?.pushViewController(tabBarController, animated: true)
+                    }
+                    else {
+                        println("Unable to follow")
+                    }
+                })
             }
             else {
                 println("sign up failure")
             }
         }
+    }
+    
+    func follow(user: PFUser!, completionHandler:(error: NSError?) -> ())
+    {
+        var relation = PFUser.currentUser().relationForKey("following")
+        relation.addObject(user)
+        PFUser.currentUser().saveInBackgroundWithBlock({
+            (success, error) -> Void in
+            
+            completionHandler(error: error)
+        })
     }
 
 }
