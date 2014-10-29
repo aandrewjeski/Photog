4a95e09f4f9b97670df334f8515506edeb70364b
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-10-29 17:47:28 -0400

    Day 11: Created FeedViewController and supporting files

---
+++ Photog/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Photog
+//
+//  Created by Adam Andrewjeski on 10/29/14.
+//  Copyright (c) 2014 One Month iOS. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    var item: PFObject?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
